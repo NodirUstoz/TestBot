01f951af1c8b2b3b07b371168e99b9c0068c51a5
Nodir Odilov <tuproqqalam@gmail.com>
2025-08-16 15:02:25 +0500

    Tajriba uchun!

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 TOKEN='7195835984:AAHZeYj1G9f58HRWe7VwgwBikgj2JUy9TPU'
+# Bu token ishlamaydi. O'quvchilarga tokenni githubga chiqarganda qanday qilib uni BotFather da o'zgartirish o'rgatildi. Tajriba uchun!
